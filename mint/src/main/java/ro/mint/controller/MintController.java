84f6d1d31934c52bc276f753cd97dcc01139646e
marius <jianuma@gmail.com>
2018-06-23 23:48:30 +0300

    testare request response

--- mint/src/main/java/ro/mint/controller/MintController.java
+++ mint/src/main/java/ro/mint/controller/MintController.java
@@ -1,10 +1,20 @@
 package ro.mint.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/")
+@RequestMapping("/mint")
 public class MintController {
 
+
+    @PostMapping
+    @RequestMapping("/testReq")
+    String testReq(Principal principal) {
+        return "This is a response from the server.";
+
+    }
 }

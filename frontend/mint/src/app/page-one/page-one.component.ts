5f2ea8377f345d62720b68282a7211c909fd93c5
Nicu Socol <32669138+ngsocol@users.noreply.github.com>
2018-06-24 14:34:39 +0300

    test http

---
+++ frontend/mint/src/app/page-one/page-one.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-page-one',
+  templateUrl: './page-one.component.html',
+  styleUrls: ['./page-one.component.css']
+})
+export class PageOneComponent implements OnInit {
+  response;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('http://localhost:8080/testit').subscribe(response => {
+      console.log(response);
+      this.response = JSON.stringify(response);
+    });
+  }
+}
